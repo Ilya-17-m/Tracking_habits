2f2b6b5f36e01e794c4c33308b52667955e0867c
Ilya-17-m <ilya.melnichuk.2007@mail.ru>
2026-04-15 11:53:14 +0500

    Update config.py

--- frontend/config.py
+++ frontend/config.py
@@ -1,6 +1,45 @@
 import os
 import logging.config
 from dotenv import load_dotenv
+from telebot import TeleBot, StateMemoryStorage
+
+load_dotenv()
+
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_URL = os.getenv("API_URL")
+
+bot = TeleBot(BOT_TOKEN, state_storage=StateMemoryStorage())
+
+
+logging.config.dictConfig({
+    "version": 1,
+    "disable_existing_loggers": False,
+
+    "formatters": {
+        "console": {
+            "format": "%(asctime)s %(levelname)s: %(name)s %(module)s %(lineno)s %(message)s"
+        },
+    },
+
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "console",
+        },
+    },
+
+    "loggers": {
+        "": {
+            "level": "INFO",
+            "handlers": [
+                "console",
+            ]
+        }
+    }
+})import os
+import logging.config
+from dotenv import load_dotenv
 
 load_dotenv()
 
