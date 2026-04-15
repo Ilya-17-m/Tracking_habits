b5ba5e1180264545cbfaad359593a580c66fa2cf
Ilya-17-m <ilya.melnichuk.2007@mail.ru>
2026-04-15 11:49:47 +0500

    Update database_conf.py

--- backend/database_conf.py
+++ backend/database_conf.py
@@ -16,6 +16,35 @@ async_session = async_sessionmaker(async_engine, expire_on_commit=False)
 @asynccontextmanager
 async def lifespan(apps: FastAPI):
     async with async_engine.begin() as conn:
+        #await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+
+async def get_session():
+    async with async_session() as session:
+        yield session
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]from contextlib import asynccontextmanager
+from fastapi import Depends, FastAPI
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from typing import Annotated
+
+from .models import Base
+from .config import password, user, database
+
+async_engine = create_async_engine(
+    f"postgresql+asyncpg://{user}:{password}@localhost:5432/{database}"
+)
+
+async_session = async_sessionmaker(async_engine, expire_on_commit=False)
+
+
+@asynccontextmanager
+async def lifespan(apps: FastAPI):
+    async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
     yield
