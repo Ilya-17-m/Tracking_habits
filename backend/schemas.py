9dd5099c00b362dbc9a5effc8aafef09b2da62d2
Ilya-17-m <ilya.melnichuk.2007@mail.ru>
2026-04-15 11:49:11 +0500

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,4 +1,40 @@
 from pydantic import BaseModel
+
+
+class ChangeHabitSchema(BaseModel):
+    title: str
+    new_title: str
+    status: bool
+    time: str
+    object: str
+    user_id: int
+
+
+class CreateHabitSchema(BaseModel):
+    title: str
+    time: str
+    user_id: int
+
+
+class DeleteHabitSchema(BaseModel):
+    title: str
+
+
+class ProfileSchema(BaseModel):
+    username: str
+    last_name: str
+    first_name: str
+    chat_id: int
+    user_id: int
+
+
+class UserLoginSchema(BaseModel):
+    username: str
+    last_name: str
+    first_name: str
+    chat_id: int
+    user_id: int
+from pydantic import BaseModel
 from datetime import time
 
 
